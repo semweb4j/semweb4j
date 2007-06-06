9988f87cd67fb53821d63fba7f78fa60ea02d52a
Max Völkel <max.voelkel@calpano.com>
2007-06-06 09:19:00 +0000

    need base.impl.test.:4.4.6 SNAPSHOT now

--- org.semweb4j.rdf2go.impl.jena24/src/test/java/org/ontoware/rdf2go/impl/jena24/AllTests.java
+++ org.semweb4j.rdf2go.impl.jena24/src/test/java/org/ontoware/rdf2go/impl/jena24/AllTests.java
@@ -1,10 +1,10 @@
 package org.ontoware.rdf2go.impl.jena24;
 
-import org.ontoware.rdf2go.model.ModelSetTest;
-import org.ontoware.rdf2go.model.ModelTest;
-
 import junit.framework.Test;
 import junit.framework.TestSuite;
+
+import org.ontoware.rdf2go.model.ModelTest;
+import org.ontoware.rdf2go.testdata.TestData;
 
 
 
@@ -13,12 +13,15 @@ public class AllTests
 
 	public static Test suite()
 	{
+		
+		TestData.main(null);
+		
 		TestSuite suite = new TestSuite(
 				"Test for org.ontoware.rdf2go.impl.jena24");
 
 		// impl.jena24
 		suite.addTestSuite(ModelTest.class);
-		suite.addTestSuite(ModelSetTest.class);
+		//suite.addTestSuite(ModelSetTest.class);
 		suite.addTestSuite(TransactionTest.class);
 		suite.addTestSuite(DataTypeTesting.class);
 		return suite;
