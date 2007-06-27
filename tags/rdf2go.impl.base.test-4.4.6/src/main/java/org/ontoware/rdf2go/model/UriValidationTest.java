1691696c645e1afd9f0742498f050d442c57a10c
max.at.xam.de <max.at.xam.de@3b2a6c2a-ec1e-0410-ad68-6d684d8aefd9>
2007-06-27 23:11:14 +0000

    [maven-release-plugin]  copy for tag rdf2go.impl.base.test-4.4.6

---
+++ tags/rdf2go.impl.base.test-4.4.6/src/main/java/org/ontoware/rdf2go/model/UriValidationTest.java
@@ -0,0 +1,51 @@
+package org.ontoware.rdf2go.model;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import junit.framework.TestCase;
+
+public class UriValidationTest extends TestCase {
+
+	String[] validURIs = { "http://xam.de", "urn:x-cds:item-1" };
+
+	/**
+	 * urn:gunnar is not a valid URI, because it pretends to be a URN, but those
+	 * have the syntax ns:part:part. 
+	 */
+	String[] invalidURIs = { "123","urn:gunnar", "Ping",
+			"http://300.300.300.300/%1234", "���", "&xtc;" };
+
+	public void testJavaNetUri() {
+		// expect no error
+		for (String s : validURIs) {
+			try {
+				@SuppressWarnings("unused")
+				URI uri = new URI(s);
+			} catch (URISyntaxException e) {
+				assertFalse("No exceptions (at least on my machine)", true);
+				throw new RuntimeException(e);
+			}
+		}
+
+		// TODO this test doesn't work. 
+		
+//		// expect errors
+//		for (String s : invalidURIs) {
+//			boolean error = false;
+//			try {
+//				@SuppressWarnings("unused")
+//				URI uri = new URI(s);
+//				
+//			
+//				// exception should be thrown
+////				assertFalse("Expect exception because '" + s
+////						+ "' is not a valid URI", true);
+//			} catch (URISyntaxException e) {
+//				// ok
+//				error = true;
+//			}
+//			assertTrue("got no URISyntaxException", error);
+//		}
+	}
+}
