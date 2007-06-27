1b3d7c9af94a193949ff257ad45133704d71a007
max.at.xam.de <max.at.xam.de@3b2a6c2a-ec1e-0410-ad68-6d684d8aefd9>
2007-06-27 14:36:02 +0000

    [maven-release-plugin]  copy for tag rdf2go.impl.base-4.4.6

---
+++ tags/rdf2go.impl.base-4.4.6/src/main/java/org/ontoware/rdf2go/model/impl/NotifyingModelLayer.java
@@ -0,0 +1,301 @@
+package org.ontoware.rdf2go.model.impl;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.ontoware.rdf2go.exception.ModelRuntimeException;
+import org.ontoware.rdf2go.model.Diff;
+import org.ontoware.rdf2go.model.Model;
+import org.ontoware.rdf2go.model.ModelChangedListener;
+import org.ontoware.rdf2go.model.NotifyingModel;
+import org.ontoware.rdf2go.model.Statement;
+import org.ontoware.rdf2go.model.TriplePattern;
+import org.ontoware.rdf2go.model.node.Node;
+import org.ontoware.rdf2go.model.node.Resource;
+import org.ontoware.rdf2go.model.node.URI;
+
+/**
+ * Adds notifying capabilites to existing models.
+ * @author voelkel
+ *
+ */
+public class NotifyingModelLayer extends DelegatingModel implements
+		NotifyingModel {
+
+	private Model model;
+	
+	public NotifyingModelLayer( Model model ) {
+		this.model = model;
+	}
+
+	// /////////////////////////////////
+	// override methods to be able to notify
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.common.CommonModelWriter#addAll(org.ontoware.rdf2go.core.common.CommonModelReader)
+	 */
+	public void addAll(Iterator<? extends Statement> other)
+			throws ModelRuntimeException {
+		while (other.hasNext()) {
+			this.addStatement(other.next());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#addStatement(org.ontoware.rdf2go.core.node.Resource,
+	 *      org.ontoware.rdf2go.core.node.URI,
+	 *      org.ontoware.rdf2go.core.node.Node)
+	 */
+	public void addStatement(Resource subject, URI predicate, Node object)
+			throws ModelRuntimeException {
+		this.addStatement(model.createStatement(subject, predicate, object));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#addStatement(org.ontoware.rdf2go.core.node.Resource,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String,
+	 *      java.lang.String)
+	 */
+	public void addStatement(Resource subject, URI predicate, String literal,
+			String languageTag) throws ModelRuntimeException {
+		this.addStatement(model.createStatement(subject, predicate, model
+				.createLanguageTagLiteral(literal, languageTag)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#addStatement(org.ontoware.rdf2go.core.node.Resource,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI)
+	 */
+	public void addStatement(Resource subject, URI predicate, String literal,
+			URI datatypeURI) throws ModelRuntimeException {
+		this.addStatement(subject, predicate, model
+				.createDatatypeLiteral(literal, datatypeURI));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#addStatement(org.ontoware.rdf2go.core.node.Resource,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String)
+	 */
+	public void addStatement(Resource subject, URI predicate, String literal)
+			throws ModelRuntimeException {
+		this.addStatement(model.createStatement(subject, predicate, model
+				.createPlainLiteral(literal)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.common.CommonModelWriter#addStatement(S)
+	 */
+	public void addStatement(Statement statement) throws ModelRuntimeException {
+		for (ModelChangedListener listener : modelChangeListener.keySet()) {
+			TriplePattern pattern = modelChangeListener.get(listener);
+			if (pattern == null || pattern.matches(statement)) {
+				listener.addedStatement(statement);
+			}
+		}
+		model.addStatement(statement);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#addStatement(java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String,
+	 *      java.lang.String)
+	 */
+	public void addStatement(String subjectURIString, URI predicate,
+			String literal, String languageTag) throws ModelRuntimeException {
+		this.addStatement(model.createStatement(model
+				.createURI(subjectURIString), predicate, model
+				.createLanguageTagLiteral(literal, languageTag)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#addStatement(java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI)
+	 */
+	public void addStatement(String subjectURIString, URI predicate,
+			String literal, URI datatypeURI) throws ModelRuntimeException {
+		this.addStatement(model.createStatement(model
+				.createURI(subjectURIString), predicate, model
+				.createDatatypeLiteral(literal, datatypeURI)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#addStatement(java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String)
+	 */
+	public void addStatement(String subjectURIString, URI predicate,
+			String literal) throws ModelRuntimeException {
+		this.addStatement(model.createStatement(model
+				.createURI(subjectURIString), predicate, model
+				.createPlainLiteral(literal)));
+	}
+
+	/////////////// remove
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.common.CommonModelWriter#removeAll(org.ontoware.rdf2go.core.common.CommonModelReader)
+	 */
+	public void removeAll(Iterator<? extends Statement> other)
+			throws ModelRuntimeException {
+		while (other.hasNext()) {
+			this.removeStatement(other.next());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#removeStatement(org.ontoware.rdf2go.core.node.Resource,
+	 *      org.ontoware.rdf2go.core.node.URI,
+	 *      org.ontoware.rdf2go.core.node.Node)
+	 */
+	public void removeStatement(Resource subject, URI predicate, Node object)
+			throws ModelRuntimeException {
+		this.removeStatement(model.createStatement(subject, predicate, object));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#removeStatement(org.ontoware.rdf2go.core.node.Resource,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String,
+	 *      java.lang.String)
+	 */
+	public void removeStatement(Resource subject, URI predicate, String literal,
+			String languageTag) throws ModelRuntimeException {
+		this.removeStatement(model.createStatement(subject, predicate, model
+				.createLanguageTagLiteral(literal, languageTag)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#removeStatement(org.ontoware.rdf2go.core.node.Resource,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI)
+	 */
+	public void removeStatement(Resource subject, URI predicate, String literal,
+			URI datatypeURI) throws ModelRuntimeException {
+		this.removeStatement(subject, predicate, model
+				.createDatatypeLiteral(literal, datatypeURI));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#removeStatement(org.ontoware.rdf2go.core.node.Resource,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String)
+	 */
+	public void removeStatement(Resource subject, URI predicate, String literal)
+			throws ModelRuntimeException {
+		this.removeStatement(model.createStatement(subject, predicate, model
+				.createPlainLiteral(literal)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.common.CommonModelWriter#removeStatement(S)
+	 */
+	public void removeStatement(Statement statement) throws ModelRuntimeException {
+		for (ModelChangedListener listener : modelChangeListener.keySet()) {
+			TriplePattern pattern = modelChangeListener.get(listener);
+			if (pattern == null || pattern.matches(statement)) {
+				listener.removedStatement(statement);
+			}
+		}
+		model.removeStatement(statement);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#removeStatement(java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String,
+	 *      java.lang.String)
+	 */
+	public void removeStatement(String subjectURIString, URI predicate,
+			String literal, String languageTag) throws ModelRuntimeException {
+		this.removeStatement(model.createStatement(model
+				.createURI(subjectURIString), predicate, model
+				.createLanguageTagLiteral(literal, languageTag)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#removeStatement(java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI)
+	 */
+	public void removeStatement(String subjectURIString, URI predicate,
+			String literal, URI datatypeURI) throws ModelRuntimeException {
+		this.removeStatement(model.createStatement(model
+				.createURI(subjectURIString), predicate, model
+				.createDatatypeLiteral(literal, datatypeURI)));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ontoware.rdf2go.core.triple.ModelWriter#removeStatement(java.lang.String,
+	 *      org.ontoware.rdf2go.core.node.URI, java.lang.String)
+	 */
+	public void removeStatement(String subjectURIString, URI predicate,
+			String literal) throws ModelRuntimeException {
+		this.removeStatement(model.createStatement(model
+				.createURI(subjectURIString), predicate, model
+				.createPlainLiteral(literal)));
+	}
+
+	//////////// diff
+	
+	public void update(Diff diff) throws ModelRuntimeException {
+		for (ModelChangedListener listener : modelChangeListener.keySet()) {
+				listener.performedUpdate(diff);
+		}
+		model.update(diff);
+	}
+
+	
+	// //////////////////////////////////
+	// implement NotifyingModel
+
+	private Map<ModelChangedListener, TriplePattern> modelChangeListener = new HashMap<ModelChangedListener, TriplePattern>();
+
+	public void addModelChangedListener(ModelChangedListener listener,
+			TriplePattern pattern) {
+		modelChangeListener.put(listener, pattern);
+	}
+
+	public void addModelChangedListener(ModelChangedListener listener) {
+		modelChangeListener.put(listener, null);
+	}
+
+	public void removeModelChangedListener(ModelChangedListener listener) {
+		modelChangeListener.remove(listener);
+	}
+
+}
---
+++ tags/rdf2go.impl.base-4.4.6/src/main/java/org/ontoware/rdf2go/model/node/impl/BlankNodeImpl.java
@@ -0,0 +1,80 @@
+/*
+ * LICENSE INFORMATION
+ * Copyright 2005-2007 by FZI (http://www.fzi.de).
+ * Licensed under a BSD license (http://www.opensource.org/licenses/bsd-license.php)
+ * <OWNER> = Max Völkel
+ * <ORGANIZATION> = FZI Forschungszentrum Informatik Karlsruhe, Karlsruhe, Germany
+ * <YEAR> = 2007
+ * 
+ * Project information at http://semweb4j.org/rdf2go
+ */
+package org.ontoware.rdf2go.model.node.impl;
+
+import org.ontoware.rdf2go.model.node.BlankNode;
+import org.ontoware.rdf2go.model.node.Node;
+import org.ontoware.rdf2go.model.node.URI;
+import org.ontoware.rdf2go.model.node.Variable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class BlankNodeImpl extends ResourceImpl implements BlankNode {
+
+	private static final Logger log = LoggerFactory.getLogger(Variable.class);
+
+	private Object underlyingBlankNode;
+
+	/** This method should only be called by RDF2Go implmentations */
+	public BlankNodeImpl(Object underlyingBlankNode) {
+		this.underlyingBlankNode = underlyingBlankNode;
+	}
+
+	/** This method should only be called by RDF2Go implmentations */
+	public Object getUnderlyingBlankNode() {
+		return this.underlyingBlankNode;
+	}
+
+	public boolean equals(Object other) {
+		return other instanceof BlankNodeImpl
+				&& this.getUnderlyingBlankNode().equals(
+						((BlankNodeImpl) other).getUnderlyingBlankNode());
+	}
+	
+	public String toString()
+	{
+		return underlyingBlankNode.toString();
+	}
+	
+	public URI asURI() throws ClassCastException {
+		throw new ClassCastException("Cannot cast a BlankNode to a URI");
+
+	}
+
+	public BlankNode asBlankNode() throws ClassCastException {
+		return this;
+	}
+
+	public int hashCode() {
+		return underlyingBlankNode.hashCode();
+	}
+
+	public int compareTo( Node other ) {
+		if (other instanceof BlankNode) {
+			if (this.equals(other)) return 0;
+			else {
+				// define somehow a sorting order on blank nodes
+				return this.hashCode() - other.hashCode();
+			}
+		}
+		else {
+			// sort by type
+			return NodeUtils.compareByType(this, other);
+		}
+	}
+
+	public String toSPARQL() throws NotImplementedException {
+		log.warn("Variable (Singleton) should not be used for SPARQL queries");
+		throw new NotImplementedException();
+	}
+}
