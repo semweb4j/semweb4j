6cf9bd0f7117587e318cb85aaefd5283aa72eb2a
max.at.xam.de <max.at.xam.de@3b2a6c2a-ec1e-0410-ad68-6d684d8aefd9>
2008-01-21 16:45:44 +0000

    Migration to RDF2Go 4.5.3 and RDFReactor.runtime 4.4.15.

--- trunk/org.semweb4j.rdfreactor.generator/src/test/java/org/ontoware/rdfreactor/generator/MicrobridgeTestGenerator.java
+++ trunk/org.semweb4j.rdfreactor.generator/src/test/java/org/ontoware/rdfreactor/generator/MicrobridgeTestGenerator.java
@@ -10,8 +10,15 @@ public class MicrobridgeTestGenerator {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		CodeGenerator.generate("./data/tag.n3", "./test-src", "org.ontoware.rdfreactor.test.tag",
-				"RDFS", true, true,"");
+		CodeGenerator.generate("P:/swecr/org.semanticdesktop.swecr.model/src/main/resources/swcm.n3", 
+				"./src/test/java", 
+				"org.semanticdesktop.swecr.model.rdf.generated",
+				"RDFS", 
+				true, 
+				true,
+				"");
+//		CodeGenerator.generate("./src/test/resources/TagFilter.n3", "./src/test/java", "gen.org.ontoware.rdfreactor.test.tag",
+//				"RDFS", false, true,"");
 	}
 
 }
