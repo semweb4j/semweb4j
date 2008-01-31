b51eef3efc912dcc92e9d0267352ce76ed40b9a1
max.at.xam.de <max.at.xam.de@3b2a6c2a-ec1e-0410-ad68-6d684d8aefd9>
2008-01-31 16:41:06 +0000

    Added search&replace tool

--- trunk/org.semweb4j.rdf2go.impl.util/src/main/java/org/ontoware/rdf2go/util/transform/Transformer.java
+++ trunk/org.semweb4j.rdf2go.impl.util/src/main/java/org/ontoware/rdf2go/util/transform/Transformer.java
@@ -1,16 +1,12 @@
 package org.ontoware.rdf2go.util.transform;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.ontoware.aifbcommons.collection.ClosableIterator;
-import org.ontoware.rdf2go.RDF2Go;
-import org.ontoware.rdf2go.model.Diff;
 import org.ontoware.rdf2go.model.Model;
-import org.ontoware.rdf2go.model.Statement;
-import org.ontoware.rdf2go.model.impl.DiffImpl;
 import org.ontoware.rdf2go.model.node.URI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Transforms RDF syntactically according to configuration, e.g. renaming
@@ -21,6 +17,8 @@ import org.ontoware.rdf2go.model.node.URI;
  */
 public class Transformer {
 
+	private static Logger log = LoggerFactory.getLogger(Transformer.class);
+	
 	/**
 	 * @param model
 	 * @param rules -
@@ -28,59 +26,12 @@ public class Transformer {
 	 */
 	public static void transform(Model model, Map<String, URI> namespaceMap,
 			List<TransformerRule> rules) {
-		for (TransformerRule rule : rules) {
-			if (rule instanceof SearchReplaceRule) {
-				SearchReplaceRule srRule = (SearchReplaceRule) rule;
-				applySearchReplaceRule(model, namespaceMap, srRule.getSearch(),
-						srRule.getRemove(), srRule.getAdd());
-			} else if (rule instanceof InferenceRule) {
-				InferenceRule infRule = (InferenceRule) rule;
-				applyInferenceRule(model, namespaceMap, infRule.getSearch(),
-						infRule.getAdd());
-			} else
-				throw new IllegalArgumentException(
-						"Cannot handle rules of type " + rule.getClass());
+		
+		for (int i = 0; i < rules.size(); i++) {
+			log.debug("Executing rule "+i+" -------- "+rules.get(i).getClass());
+			rules.get(i).applyRule(model, namespaceMap);
 		}
-	}
-
-	public static void applySearchReplaceRule(Model model,
-			Map<String, URI> namespaceMap, String where,
-			String constructRemove, String constructAdd) {
-		Model remove = RDF2Go.getModelFactory().createModel();
-		remove.open();
-		ClosableIterator<Statement> it = model.sparqlConstruct(
-				toSparqlConstruct(namespaceMap, constructRemove, where))
-				.iterator();
-		remove.addAll((Iterator<? extends Statement>) it);
-		it.close();
-
-		Model add = RDF2Go.getModelFactory().createModel();
-		add.open();
-		it = model.sparqlConstruct(
-				toSparqlConstruct(namespaceMap, constructAdd, where))
-				.iterator();
-		add.addAll((Iterator<? extends Statement>) it);
-		it.close();
-
-		Diff diff = new DiffImpl(add.iterator(), remove.iterator());
-		model.update(diff);
-	}
-
-	public static void applyInferenceRule(Model model,
-			Map<String, URI> namespaceMap, String where, String constructAdd) {
-		Model remove = RDF2Go.getModelFactory().createModel();
-		remove.open();
-
-		Model add = RDF2Go.getModelFactory().createModel();
-		add.open();
-		ClosableIterator<Statement> it = model.sparqlConstruct(
-				toSparqlConstruct(namespaceMap, constructAdd, where))
-				.iterator();
-		add.addAll((Iterator<? extends Statement>) it);
-		it.close();
-
-		Diff diff = new DiffImpl(add.iterator(), remove.iterator());
-		model.update(diff);
+		
 	}
 
 	public static String toSparqlConstruct(Map<String, URI> namespaceMap,
@@ -95,6 +46,7 @@ public class Transformer {
 		query.append("} WHERE {\n");
 		query.append(where).append("\n");
 		query.append("}\n");
+		log.debug("query \n"+query);
 		return query.toString();
 	}
 }
--- trunk/org.semweb4j.rdf2go.impl.util/src/main/java/org/ontoware/rdf2go/util/transform/TransformerRule.java
+++ trunk/org.semweb4j.rdf2go.impl.util/src/main/java/org/ontoware/rdf2go/util/transform/TransformerRule.java
@@ -1,5 +1,12 @@
 package org.ontoware.rdf2go.util.transform;
 
-public interface TransformerRule {
+import java.util.Map;
 
+import org.ontoware.rdf2go.model.Model;
+import org.ontoware.rdf2go.model.node.URI;
+
+public interface TransformerRule {
+	
+	void applyRule( Model model, Map<String,URI> namespaceMap );
+	
 }
