3528664145caa234d9cc3e5357d3a83c4e9cc6f7
Max Völkel <max.voelkel@calpano.com>
2008-04-15 10:03:35 +0000

    removed deprecated methods

--- org.semweb4j.rdf2go.api/src/main/java/org/ontoware/rdf2go/model/node/DatatypeLiteral.java
+++ org.semweb4j.rdf2go.api/src/main/java/org/ontoware/rdf2go/model/node/DatatypeLiteral.java
@@ -10,7 +10,6 @@
  */
 package org.ontoware.rdf2go.model.node;
 
-import java.util.Date;
 
 /**
  * RDF Literal representation of a datatype (usually xml schema datatype)
@@ -29,42 +28,5 @@ public interface DatatypeLiteral extends Literal {
 	 * @return the URI of the datatype
 	 */
 	public URI getDatatype();
-
-	/**
-	 * 
-	 * @return
-	 * @throws NumberFormatException
-	 *             if the literal value could not be parsed
-	 * @throws ClassCastException
-	 *             if the Literal is not an xsd:integer or a subtype of it
-	 * @deprecated this should be part of a utility, not the core API. Use
-	 *             org.ontoware.rdf2go.util.TypeConverter from rdf2go.base.impl
-	 *             instead
-	 */
-	public int asInt() throws NumberFormatException, ClassCastException;
-
-	/**
-	 * @return Parses the string argument as a boolean. The boolean returned
-	 *         represents the value true if the string argument is not null and
-	 *         is equal, ignoring case, to the string "true".
-	 * @throws ClassCastException
-	 *             is the literal is not a DatatypeLiteral with a datatype of
-	 *             xsd:boolean.
-	 * @deprecated this should be part of a utility, not the core API. Use
-	 *             org.ontoware.rdf2go.util.TypeConverter from rdf2go.base.impl
-	 *             instead
-	 */
-	public boolean asBoolean() throws ClassCastException;
-
-	/**
-	 * @return a java.util.Date representing the literal value
-	 * @throws ClassCastException
-	 *             if the Literals is not a DatatypeLiteral with datatype of
-	 *             xsd:dateTime, xsd:date or xsd:time.
-	 * @deprecated this should be part of a utility, not the core API. Use
-	 *             org.ontoware.rdf2go.util.TypeConverter from rdf2go.base.impl
-	 *             instead
-	 */
-	public Date asDate() throws ClassCastException;
 
 }
