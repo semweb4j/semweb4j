de50e73315742868afe5334a913ef32a751d0bf7
max.at.xam.de <max.at.xam.de@3b2a6c2a-ec1e-0410-ad68-6d684d8aefd9>
2008-04-30 08:53:31 +0000

    [maven-release-plugin]  copy for tag rdf2go.impl.base.test-4.6.1

---
+++ tags/rdf2go.impl.base.test-4.6.1/src/main/java/org/ontoware/rdf2go/model/AbstractModelChangeListener.java
@@ -0,0 +1,47 @@
+/*
+ * LICENSE INFORMATION
+ * Copyright 2005-2007 by FZI (http://www.fzi.de).
+ * Licensed under a BSD license (http://www.opensource.org/licenses/bsd-license.php)
+ * <OWNER> = Max V�lkel
+ * <ORGANIZATION> = FZI Forschungszentrum Informatik Karlsruhe, Karlsruhe, Germany
+ * <YEAR> = 2007
+ * 
+ * Project information at http://semweb4j.org/rdf2go
+ */
+
+package org.ontoware.rdf2go.model;
+
+import java.util.Iterator;
+
+abstract class AbstractModelChangeListener implements ModelChangedListener
+{
+	public void addedStatement(Statement statement)
+	{
+		NotifyingModelTest.fail();
+	}
+
+	public void addedStatements(Iterator<? extends Statement> statements)
+	{
+		NotifyingModelTest.fail();
+	}
+
+	public void performedUpdate(DiffReader diff)
+	{
+		NotifyingModelTest.fail();
+	}
+
+	public void removedStatement(Statement statement)
+	{
+		NotifyingModelTest.fail();
+	}
+
+	public void removedStatements(Iterator<? extends Statement> statements)
+	{
+		NotifyingModelTest.fail();
+	}
+	
+	public void performedUpdate(Diff diff) {
+		NotifyingModelTest.fail();
+	}
+
+}
