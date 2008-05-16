c5ceea71ac2316853ee9cf6aa41abf12df1d0e0c
werner.thiemann <contact@blue-age.de>
2008-05-16 16:08:16 +0000

    [maven-release-plugin]  copy for tag maven.skin-0.0.1

--- org.semweb4j.rdfreactor.runtime/src/main/java/org/ontoware/rdfreactor/runtime/ReactorWrapper.java
+++
@@ -1,9 +0,0 @@
-package org.ontoware.rdfreactor.runtime;
-
-import org.ontoware.rdf2go.model.Model;
-
-public interface ReactorWrapper extends ResourceEntity {
-
-	public abstract Model getModel();
-
-}
