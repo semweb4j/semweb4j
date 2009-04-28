7735a246b716b9789fd7c185f6ccf789438f14be
Max Völkel <max.voelkel@calpano.com>
2009-04-28 16:37:55 +0000

    Changed pom so that RDF2Go includes some source code in the JAR. This will allow in the end, that RDF2Go is used in GWT, which means, it can be used in Javascript. Still a long way to go for that.

--- org.semweb4j.rdf2go.api/src/main/java/org/ontoware/rdf2go/model/node/impl/AbstractBlankNodeImpl.java
+++ org.semweb4j.rdf2go.api/src/main/java/org/ontoware/rdf2go/model/node/impl/AbstractBlankNodeImpl.java
@@ -15,13 +15,8 @@ package org.ontoware.rdf2go.model.node.impl;
 import org.ontoware.rdf2go.model.node.BlankNode;
 import org.ontoware.rdf2go.model.node.Node;
 import org.ontoware.rdf2go.model.node.URI;
-import org.ontoware.rdf2go.model.node.Variable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class AbstractBlankNodeImpl extends ResourceImpl implements BlankNode {
-
-	private static final Logger log = LoggerFactory.getLogger(Variable.class);
 
 	private Object underlyingBlankNode;
 
@@ -74,8 +69,7 @@ public abstract class AbstractBlankNodeImpl extends ResourceImpl implements Blan
 	}
 
 	public String toSPARQL() throws UnsupportedOperationException {
-		log.warn("Variable (Singleton) should not be used for SPARQL queries");
-		throw new UnsupportedOperationException("Not implemented");
+		throw new UnsupportedOperationException("Variable (Singleton) cannot be used for SPARQL queries");
 	}
 
 	public abstract String getInternalID();
