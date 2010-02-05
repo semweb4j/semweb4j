7376915924640bec935917c426f82e2eacc76fde
max.at.xam.de <max.at.xam.de@3b2a6c2a-ec1e-0410-ad68-6d684d8aefd9>
2010-02-05 13:50:37 +0000

    Updated license headers to year 2010

--- trunk/org.semweb4j.rdf2go.api/src/main/java/org/ontoware/rdf2go/osgi/RDF2GoActivator.java
+++ trunk/org.semweb4j.rdf2go.api/src/main/java/org/ontoware/rdf2go/osgi/RDF2GoActivator.java
@@ -1,13 +1,15 @@
 /**
  * LICENSE INFORMATION
- * 
- * Copyright 2005-2008 by FZI (http://www.fzi.de). Licensed under a BSD license
- * (http://www.opensource.org/licenses/bsd-license.php) <OWNER> = Max Völkel
- * <ORGANIZATION> = FZI Forschungszentrum Informatik Karlsruhe, Karlsruhe,
- * Germany <YEAR> = 2008
- * 
+ *
+ * Copyright 2005-2008 by FZI (http://www.fzi.de).
+ * Licensed under a BSD license (http://www.opensource.org/licenses/bsd-license.php)
+ * <OWNER> = Max Völkel
+ * <ORGANIZATION> = FZI Forschungszentrum Informatik Karlsruhe, Karlsruhe, Germany
+ * <YEAR> = 2010
+ *
  * Further project information at http://semanticweb.org/wiki/RDF2Go
  */
+
 package org.ontoware.rdf2go.osgi;
 
 import org.ontoware.rdf2go.ModelFactory;
--- trunk/org.semweb4j.rdf2go.api/src/main/java/org/ontoware/rdf2go/util/TypeConverter.java
+++ trunk/org.semweb4j.rdf2go.api/src/main/java/org/ontoware/rdf2go/util/TypeConverter.java
@@ -1,13 +1,13 @@
 /**
  * LICENSE INFORMATION
- * 
+ *
  * Copyright 2005-2008 by FZI (http://www.fzi.de).
  * Licensed under a BSD license (http://www.opensource.org/licenses/bsd-license.php)
  * <OWNER> = Max Völkel
  * <ORGANIZATION> = FZI Forschungszentrum Informatik Karlsruhe, Karlsruhe, Germany
- * <YEAR> = 2008
- * 
- * Further project information at http://semanticweb.org/wiki/RDF2Go 
+ * <YEAR> = 2010
+ *
+ * Further project information at http://semanticweb.org/wiki/RDF2Go
  */
 
 package org.ontoware.rdf2go.util;
