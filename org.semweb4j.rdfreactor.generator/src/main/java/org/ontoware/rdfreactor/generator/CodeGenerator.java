e83aa53f88bf5c3a947e707a4c5c16049bcf8e81
Max Völkel <max.voelkel@calpano.com>
2010-02-11 09:51:13 +0000

    Fix REAC-32, beautified code generation templates to generate less Eclipse warnings

--- org.semweb4j.rdfreactor.generator/src/main/java/org/ontoware/rdfreactor/generator/CodeGenerator.java
+++ org.semweb4j.rdfreactor.generator/src/main/java/org/ontoware/rdfreactor/generator/CodeGenerator.java
@@ -9,6 +9,7 @@ import org.ontoware.rdfreactor.generator.java.JModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 /**
  * <b>CodeGenerator</b> is more of a 'model generator'
  * 
@@ -20,149 +21,129 @@ import org.slf4j.LoggerFactory;
  * 
  */
 public class CodeGenerator {
-
+	
 	public static final String GENERATOR_VERSION = "$Id$";
-
+	
 	private static Logger log = LoggerFactory.getLogger(CodeGenerator.class);
-
+	
 	public static final String SEMANTICS_RDFS = "rdfs";
-
+	
 	public static final String SEMANTICS_OWL = "owl";
-
+	
 	public static final String SEMANTICS_RDFS_AND_OWL = "rdfs+owl";
-
+	
 	/**
 	 * Generate a Java class model from the given RDFS/OWL schema file
 	 * 
-	 * @param args
-	 *            schemafilename, outdir, packagename, (RDFS|OWL)
+	 * @param args schemafilename, outdir, packagename, (RDFS|OWL)
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
 		generate(args[0], args[1], args[2], args[3], true, true, "");
 	}
-
+	
 	/**
 	 * TODO read from configuration file and respect in template This is already
 	 * used in the veloctiy template
 	 */
 	@SuppressWarnings("unused")
 	private static boolean readOnly = false;
-
+	
 	/**
-	 * @deprecated Use {@link #generate(String,String,String,Reasoning,boolean)} instead
+	 * @deprecated Use {@link #generate(String,String,String,Reasoning,boolean)}
+	 *             instead
 	 */
 	@Deprecated
-	public static void generate(String schemafilename, String outdir,
-			String packagename, Reasoning semantics, boolean skipbuiltins,
-			@SuppressWarnings("unused")
-			boolean alwaysWriteToModel) throws Exception {
-				generate(schemafilename, outdir, packagename, semantics,
-						skipbuiltins);
-			}
-
-	public static void generate(String schemafilename, String outdir,
-			String packagename, Reasoning semantics, boolean skipbuiltins) throws Exception {
-		generate(schemafilename, outdir, packagename, semantics, skipbuiltins,
-				"");
+	public static void generate(String schemafilename, String outdir, String packagename,
+	        Reasoning semantics, boolean skipbuiltins, boolean alwaysWriteToModel) throws Exception {
+		generate(schemafilename, outdir, packagename, semantics, skipbuiltins);
 	}
-
+	
+	public static void generate(String schemafilename, String outdir, String packagename,
+	        Reasoning semantics, boolean skipbuiltins) throws Exception {
+		generate(schemafilename, outdir, packagename, semantics, skipbuiltins, "");
+	}
+	
 	/**
 	 * Generate a Java class model from the given RDFS/OWL schema file
 	 * 
-	 * @param schemafilename
-	 *            a path to an rdf or owl file in N3, NT or XML syntax. File
-	 *            extension determines parsing.
-	 * @param outdir-
-	 *            e.g './src' or './gen-src'
-	 * @param packagename -
-	 *            e.g. 'org.ontoware.myname.reactor'
-	 * @param semantics -
-	 *            'rdfs', 'owl' or 'rdfs+owl' (experimental)
-	 * @param skipbuiltins
-	 *            if false, internal helper classes are re-generated. This is
-	 *            usually not needed.
-	 * @param alwaysWriteToModel
-	 *            if false, public contructors allow for the choice
+	 * @param schemafilename a path to an rdf or owl file in N3, NT or XML
+	 *            syntax. File extension determines parsing.
+	 * @param outdir- e.g './src' or './gen-src'
+	 * @param packagename - e.g. 'org.ontoware.myname.reactor'
+	 * @param semantics - 'rdfs', 'owl' or 'rdfs+owl' (experimental)
+	 * @param skipbuiltins if false, internal helper classes are re-generated.
+	 *            This is usually not needed.
+	 * @param alwaysWriteToModel if false, public contructors allow for the
+	 *            choice
 	 * @throws Exception
-	 * @deprecated Use {@link #generate(String,String,String,Reasoning,boolean,String)} instead
+	 * @deprecated Use
+	 *             {@link #generate(String,String,String,Reasoning,boolean,String)}
+	 *             instead
 	 */
 	@Deprecated
-	public static void generate(String schemafilename, String outdir,
-			String packagename, Reasoning semantics, boolean skipbuiltins,
-			boolean alwaysWriteToModel, String methodnamePrefix)
-			throws Exception {
-				generate(schemafilename, outdir, packagename, semantics,
-						skipbuiltins, methodnamePrefix);
-			}
-
+	public static void generate(String schemafilename, String outdir, String packagename,
+	        Reasoning semantics, boolean skipbuiltins, boolean alwaysWriteToModel,
+	        String methodnamePrefix) throws Exception {
+		generate(schemafilename, outdir, packagename, semantics, skipbuiltins, methodnamePrefix);
+	}
+	
 	/**
 	 * Generate a Java class model from the given RDFS/OWL schema file
 	 * 
-	 * @param schemafilename
-	 *            a path to an rdf or owl file in N3, NT or XML syntax. File
-	 *            extension determines parsing.
-	 * @param packagename -
-	 *            e.g. 'org.ontoware.myname.reactor'
-	 * @param semantics -
-	 *            'rdfs', 'owl' or 'rdfs+owl' (experimental)
-	 * @param skipbuiltins
-	 *            if false, internal helper classes are re-generated. This is
-	 *            usually not needed.
-	 * @param outdir-
-	 *            e.g './src' or './gen-src'
+	 * @param schemafilename a path to an rdf or owl file in N3, NT or XML
+	 *            syntax. File extension determines parsing.
+	 * @param packagename - e.g. 'org.ontoware.myname.reactor'
+	 * @param semantics - 'rdfs', 'owl' or 'rdfs+owl' (experimental)
+	 * @param skipbuiltins if false, internal helper classes are re-generated.
+	 *            This is usually not needed.
+	 * @param outdir- e.g './src' or './gen-src'
 	 * @throws Exception
 	 */
-	public static void generate(String schemafilename, String outdir,
-			String packagename, Reasoning semantics, boolean skipbuiltins,
-			String methodnamePrefix)
-			throws Exception {
-
+	public static void generate(String schemafilename, String outdir, String packagename,
+	        Reasoning semantics, boolean skipbuiltins, String methodnamePrefix) throws Exception {
+		
 		// first step
 		Model schemaDataModel = loadSchemaDataModel(schemafilename);
 		File outDir = new File(outdir);
-
-		generate(schemaDataModel, outDir, packagename, semantics, skipbuiltins,
-				methodnamePrefix);
+		
+		generate(schemaDataModel, outDir, packagename, semantics, skipbuiltins, methodnamePrefix);
 		schemaDataModel.close();
 	}
-
+	
 	/**
 	 * @param schemafilename
 	 * @param outdir
 	 * @param packagename
 	 * @param semantics
 	 * @param skipbuiltins
-	 * @param alwaysWriteToModel  TODO this is currently just ignored.
+	 * @param alwaysWriteToModel TODO this is currently just ignored.
 	 * @param methodnamePrefix
 	 * @throws Exception
 	 */
-	public static void generate(String schemafilename, String outdir,
-			String packagename, String semantics, boolean skipbuiltins,
-			boolean alwaysWriteToModel, String methodnamePrefix)
-			throws Exception {
-
+	public static void generate(String schemafilename, String outdir, String packagename,
+	        String semantics, boolean skipbuiltins, boolean alwaysWriteToModel,
+	        String methodnamePrefix) throws Exception {
+		
 		Reasoning reasoning;
-
-		if (semantics.equalsIgnoreCase(SEMANTICS_RDFS)) {
+		
+		if(semantics.equalsIgnoreCase(SEMANTICS_RDFS)) {
 			reasoning = Reasoning.rdfs;
-		} else if (semantics.equalsIgnoreCase(SEMANTICS_OWL)) {
+		} else if(semantics.equalsIgnoreCase(SEMANTICS_OWL)) {
 			reasoning = Reasoning.owl;
-		} else if (semantics.equalsIgnoreCase(SEMANTICS_RDFS_AND_OWL)) {
+		} else if(semantics.equalsIgnoreCase(SEMANTICS_RDFS_AND_OWL)) {
 			reasoning = Reasoning.rdfsAndOwl;
 		} else {
 			throw new RuntimeException("Unknown semantics: '" + semantics + "'");
 		}
-
-		generate(schemafilename, outdir, packagename, reasoning, skipbuiltins,
-				methodnamePrefix);
+		
+		generate(schemafilename, outdir, packagename, reasoning, skipbuiltins, methodnamePrefix);
 	}
-
+	
 	/**
 	 * 
-	 * @param modelWithSchemaData
-	 *            a Model in which the ontology to generate Java classes form is
-	 *            loaded
+	 * @param modelWithSchemaData a Model in which the ontology to generate Java
+	 *            classes form is loaded
 	 * @param outDir
 	 * @param packagename
 	 * @param semantics
@@ -170,70 +151,62 @@ public class CodeGenerator {
 	 * @param alwaysWriteToModel
 	 * @param methodnamePrefix
 	 * @throws Exception
-	 * @deprecated Use {@link #generate(Model,File,String,Reasoning,boolean,String)} instead
+	 * @deprecated Use
+	 *             {@link #generate(Model,File,String,Reasoning,boolean,String)}
+	 *             instead
 	 */
 	@Deprecated
-	public static void generate(Model modelWithSchemaData, File outDir,
-			String packagename, Reasoning semantics, boolean skipbuiltins,
-			boolean alwaysWriteToModel, String methodnamePrefix)
-			throws Exception {
-				generate(modelWithSchemaData, outDir, packagename, semantics,
-						skipbuiltins, methodnamePrefix);
-			}
-
+	public static void generate(Model modelWithSchemaData, File outDir, String packagename,
+	        Reasoning semantics, boolean skipbuiltins, boolean alwaysWriteToModel,
+	        String methodnamePrefix) throws Exception {
+		generate(modelWithSchemaData, outDir, packagename, semantics, skipbuiltins,
+		        methodnamePrefix);
+	}
+	
 	/**
 	 * 
-	 * @param modelWithSchemaData
-	 *            a Model in which the ontology to generate Java classes form is
-	 *            loaded
+	 * @param modelWithSchemaData a Model in which the ontology to generate Java
+	 *            classes form is loaded
 	 * @param outDir
 	 * @param packagename
 	 * @param semantics
 	 * @param skipbuiltins
 	 * @param methodnamePrefix
-	 * @throws IOException 
+	 * @throws IOException
 	 * @throws Exception
 	 */
-	public static void generate(Model modelWithSchemaData, File outDir,
-			String packagename, Reasoning semantics, boolean skipbuiltins,
-			String methodnamePrefix) throws IOException
-			 {
-
+	public static void generate(Model modelWithSchemaData, File outDir, String packagename,
+	        Reasoning semantics, boolean skipbuiltins, String methodnamePrefix) throws IOException {
+		
 		log.info("using semantics: " + semantics);
 		// different semantics mean different ways to create the internal model
 		JModel jm;
-
+		
 		// second step
-		if (semantics == Reasoning.rdfs) {
-			log.info("MODEL generating RDFS into " + outDir.getAbsolutePath()
-					+ " ...");
-			jm = ModelGenerator.createFromRDFS_Schema(modelWithSchemaData,
-					packagename, skipbuiltins);
-		} else if (semantics == Reasoning.owl) {
-			log
-					.info("generating OWL into " + outDir.getAbsolutePath()
-							+ " ...");
-			jm = ModelGenerator.createFromOWL(modelWithSchemaData, packagename,
-					skipbuiltins);
-		} else if (semantics == Reasoning.rdfsAndOwl) {
-			log.info("MODEL generating RDFS+OWL mix semantics into "
-					+ outDir.getAbsolutePath() + " ...");
-			jm = ModelGenerator.createFromRDFS_AND_OWL(modelWithSchemaData,
-					packagename, skipbuiltins);
+		if(semantics == Reasoning.rdfs) {
+			log.info("MODEL generating RDFS into " + outDir.getAbsolutePath() + " ...");
+			jm = ModelGenerator.createFromRDFS_Schema(modelWithSchemaData, packagename,
+			        skipbuiltins);
+		} else if(semantics == Reasoning.owl) {
+			log.info("generating OWL into " + outDir.getAbsolutePath() + " ...");
+			jm = ModelGenerator.createFromOWL(modelWithSchemaData, packagename, skipbuiltins);
+		} else if(semantics == Reasoning.rdfsAndOwl) {
+			log.info("MODEL generating RDFS+OWL mix semantics into " + outDir.getAbsolutePath()
+			        + " ...");
+			jm = ModelGenerator.createFromRDFS_AND_OWL(modelWithSchemaData, packagename,
+			        skipbuiltins);
 		} else {
-			throw new RuntimeException("Can't handle the semantics of '"
-					+ semantics + "'");
+			throw new RuntimeException("Can't handle the semantics of '" + semantics + "'");
 		}
-
+		
 		// close schema model
 		modelWithSchemaData.close();
 		
 		// third and final step
 		log.info("write using sourceCodeWriter");
-		SourceCodeWriter.write(jm, outDir, SourceCodeWriter.TEMPLATE_CLASS,
-				methodnamePrefix);
+		SourceCodeWriter.write(jm, outDir, SourceCodeWriter.TEMPLATE_CLASS, methodnamePrefix);
 	}
-
+	
 	/**
 	 * load & deanonymize a schema file into a Jena RDF model
 	 * 
@@ -241,13 +214,12 @@ public class CodeGenerator {
 	 * @return Jena RDF Model loaded from given filename
 	 * @throws Exception
 	 */
-	public static Model loadSchemaDataModel(String schemafilename)
-			throws Exception {
+	public static Model loadSchemaDataModel(String schemafilename) throws Exception {
 		File schemaFile = new File(schemafilename);
 		log.info("loading from " + schemaFile.getCanonicalPath());
 		Model schemaDataModel = ModelReaderUtils.read(schemafilename);
 		return schemaDataModel;
-
+		
 	}
-
+	
 }
