c1d4fb57c58df6068e1d2987ab465d1c18be1027
Max Völkel <max.voelkel@calpano.com>
2010-04-23 15:21:56 +0000

    Applied patch from Kaspar Fischer - fixed Jena 2.6.2 support.

---
+++ org.semweb4j.rdf2go.impl.jena26/src/main/java/org/ontoware/rdf2go/impl/jena26/QueryIterator.java
@@ -0,0 +1,63 @@
+package org.ontoware.rdf2go.impl.jena26;
+
+import org.ontoware.aifbcommons.collection.ClosableIterator;
+import org.ontoware.rdf2go.exception.ModelRuntimeException;
+import org.ontoware.rdf2go.model.QueryRow;
+import org.ontoware.rdf2go.model.impl.QueryRowImpl;
+
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+
+public class QueryIterator implements ClosableIterator<QueryRow>
+{
+
+  private final ResultSet resultSet;
+
+  private final QueryResultTableImpl table;
+
+  public QueryIterator(QueryResultTableImpl table, ResultSet results)
+  {
+    this.resultSet = results;
+    this.table = table;
+  }
+
+  public boolean hasNext()
+  {
+    return this.resultSet.hasNext();
+  }
+
+  @SuppressWarnings("null")
+  /*
+   * patch from garpinc against nulls - http://issues.semweb4j.org/browse/RTGO-65
+   */
+  public QueryRow next()
+  {
+    QuerySolution qs = this.resultSet.nextSolution();
+    QueryRowImpl row = new QueryRowImpl();
+    for (String v : this.table.getVariables())
+    {
+      RDFNode node = qs.get(v);
+      assert node != null : "null node for varname " + v + ". Do you have unbound variables in the query?";
+      try
+      {
+        row.put(v, TypeConversion.toRDF2Go((node == null ? null : node.asNode())));
+      }
+      catch (ModelRuntimeException e)
+      {
+        throw new ModelRuntimeException(e);
+      }
+    }
+    return row;
+  }
+
+  public void remove()
+  {
+    this.resultSet.remove();
+  }
+
+  public void close()
+  {
+  }
+
+}
