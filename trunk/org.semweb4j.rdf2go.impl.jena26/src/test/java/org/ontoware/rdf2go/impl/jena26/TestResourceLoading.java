ba61c6fff44751c242adffd35eeec0b12614d827
max.at.xam.de <max.at.xam.de@3b2a6c2a-ec1e-0410-ad68-6d684d8aefd9>
2010-04-23 15:22:10 +0000

    Applied patch from Kaspar Fischer - fixed Jena 2.6.2 support.

---
+++ trunk/org.semweb4j.rdf2go.impl.jena26/src/test/java/org/ontoware/rdf2go/impl/jena26/TestResourceLoading.java
@@ -0,0 +1,19 @@
+package org.ontoware.rdf2go.impl.jena26;
+
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestResourceLoading {
+
+	@Test
+	public void testFIle() {
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+
+		InputStream in = cl
+				.getResourceAsStream("org/ontoware/rdf2go/testdata/foaf.xml");
+		Assert.assertNotNull(in);
+	}
+
+}
