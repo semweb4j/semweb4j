b7a12ad53a43009f9bc92f7c86ac9fd67a9b3b43
Max Völkel <max.voelkel@calpano.com>
2012-04-11 20:59:20 +0000

    Re-import old jena27 code

---
+++ org.semweb4j.rdf2go.impl.jena27/src/main/examples/org/ontoware/rdf2go/example/ToN3.java
@@ -0,0 +1,23 @@
+package org.ontoware.rdf2go.example;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+
+public class ToN3 {
+	
+	/**
+	 * @param args ..
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		Model m = ModelFactory.createDefaultModel();
+		m.read(new FileInputStream(new File(
+		        "P:\\__sirdf\\rdf2go\\src\\org\\ontoware\\rdf2go\\example\\thiemann.foaf.rdf.xml")),
+		        "");
+		m.write(System.out, "N-TRIPLES");
+	}
+}
