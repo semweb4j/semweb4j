98b33f3b3e43ae0504b1bb32a5ad7924b562a52f
Max Völkel <max.voelkel@calpano.com>
2012-04-12 08:59:14 +0000

    Fixed SyntaxTest

--- org.semweb4j.rdf2go.api/src/test/java/org/ontoware/rdf2go/model/SyntaxTest.java
+++ org.semweb4j.rdf2go.api/src/test/java/org/ontoware/rdf2go/model/SyntaxTest.java
@@ -1,12 +1,11 @@
 /**
  * LICENSE INFORMATION
- *
- * Copyright 2005-2008 by FZI (http://www.fzi.de).
- * Licensed under a BSD license (http://www.opensource.org/licenses/bsd-license.php)
- * <OWNER> = Max Völkel
- * <ORGANIZATION> = FZI Forschungszentrum Informatik Karlsruhe, Karlsruhe, Germany
- * <YEAR> = 2010
- *
+ * 
+ * Copyright 2005-2008 by FZI (http://www.fzi.de). Licensed under a BSD license
+ * (http://www.opensource.org/licenses/bsd-license.php) <OWNER> = Max Völkel
+ * <ORGANIZATION> = FZI Forschungszentrum Informatik Karlsruhe, Karlsruhe,
+ * Germany <YEAR> = 2010
+ * 
  * Further project information at http://semanticweb.org/wiki/RDF2Go
  */
 
@@ -17,82 +16,93 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+
 /**
  * @author sauermann
  */
 public class SyntaxTest extends TestCase {
-
-    /**
-     * @see junit.framework.TestCase#setUp()
-     */
-    @Override
+	
+	/**
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
 	protected void setUp() throws Exception {
-        super.setUp();
-    }
-
-    /**
-     * @see junit.framework.TestCase#tearDown()
-     */
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-    /**
-     * Test method for {@link org.ontoware.rdf2go.model.Syntax#register(org.ontoware.rdf2go.model.Syntax)}.
-     */
-    public void testRegister() {
-        Syntax s = new Syntax("spargel", "application/spargel", ".spargel");
-        Syntax.register(s);
-        Syntax s1 = Syntax.forMimeType("application/spargel");
-        assertEquals(s, s1);
-    }
-
-    /**
-     * Test method for {@link org.ontoware.rdf2go.model.Syntax#forName(java.lang.String)}.
-     */
-    public void testForName() {
-        Syntax s = Syntax.forName("rdfxml");
-        assertNotNull(s);
-        assertEquals( "rdfxml", s.getName());
-    }
-
-    /**
-     * Test method for {@link org.ontoware.rdf2go.model.Syntax#forMimeType(java.lang.String)}.
-     */
-    public void testForMimeType() {
-        Syntax s = Syntax.forMimeType("application/x-turtle");
-        assertNotNull(s);
-        assertEquals( "application/x-turtle", s.getMimeType());
-    }
-
-    /**
-     * Test method for {@link org.ontoware.rdf2go.model.Syntax#unregister(org.ontoware.rdf2go.model.Syntax)}.
-     */
-    public void testUnregister() {
-        Syntax.unregister(Syntax.Ntriples);
-        Syntax s = Syntax.forName("ntriples");
-        assertNull(s);
-    }
-
-    /**
-     * Test method for {@link org.ontoware.rdf2go.model.Syntax#list()}.
-     */
-    public void testList() {
-        List<Syntax> list = Syntax.list();
-        // check overall number
-        assertEquals(6, list.size());
-        ArrayList<Syntax> l = new ArrayList<Syntax>(list);
-        // remove just a few
-        l.remove(Syntax.Ntriples);
-        l.remove(Syntax.RdfXml);
-        l.remove(Syntax.Nquads);
-        assertTrue(0 < l.size());
-    }
-    
-    public void testCreateList() {
-        List<Syntax> SYNTAXES = new ArrayList<Syntax>();
-        SYNTAXES.size();
-    }
-
+		super.setUp();
+	}
+	
+	/**
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	/**
+	 * Test method for
+	 * {@link org.ontoware.rdf2go.model.Syntax#register(org.ontoware.rdf2go.model.Syntax)}
+	 * .
+	 */
+	public void testRegister() {
+		Syntax s = new Syntax("spargel", "application/spargel", ".spargel");
+		Syntax.register(s);
+		Syntax s1 = Syntax.forMimeType("application/spargel");
+		assertEquals(s, s1);
+	}
+	
+	/**
+	 * Test method for
+	 * {@link org.ontoware.rdf2go.model.Syntax#forName(java.lang.String)}.
+	 */
+	public void testForName() {
+		Syntax.resetFactoryDefaults();
+		Syntax s = Syntax.forName("rdfxml");
+		assertNotNull(s);
+		assertEquals("rdfxml", s.getName());
+	}
+	
+	/**
+	 * Test method for
+	 * {@link org.ontoware.rdf2go.model.Syntax#forMimeType(java.lang.String)}.
+	 */
+	public void testForMimeType() {
+		Syntax.resetFactoryDefaults();
+		Syntax s = Syntax.forMimeType("application/x-turtle");
+		assertNotNull(s);
+		assertEquals("application/x-turtle", s.getMimeType());
+	}
+	
+	/**
+	 * Test method for
+	 * {@link org.ontoware.rdf2go.model.Syntax#unregister(org.ontoware.rdf2go.model.Syntax)}
+	 * .
+	 */
+	public void testUnregister() {
+		Syntax.resetFactoryDefaults();
+		Syntax.unregister(Syntax.Ntriples);
+		Syntax s = Syntax.forName("ntriples");
+		assertNull(s);
+	}
+	
+	/**
+	 * Test method for {@link org.ontoware.rdf2go.model.Syntax#list()}.
+	 */
+	public void testList() {
+		Syntax.resetFactoryDefaults();
+		List<Syntax> list = Syntax.list();
+		// check overall number
+		assertEquals(6, list.size());
+		ArrayList<Syntax> l = new ArrayList<Syntax>(list);
+		// remove just a few
+		l.remove(Syntax.Ntriples);
+		l.remove(Syntax.RdfXml);
+		l.remove(Syntax.Nquads);
+		assertTrue(0 < l.size());
+	}
+	
+	public void testCreateList() {
+		List<Syntax> SYNTAXES = new ArrayList<Syntax>();
+		SYNTAXES.size();
+	}
+	
 }
